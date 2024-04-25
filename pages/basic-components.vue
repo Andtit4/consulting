cb976d102a2b60afbd6d9a04ff4a20e51de15a67
Andtit4 <lirsitogo2021@gmail.com>
2024-04-25 16:23:46 +0000

    remove financial

--- pages/basic-components.vue
+++ pages/basic-components.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import Header from '@/layouts/header/Header.vue';
+/* import Header from '@/layouts/header/Header.vue';
 import MainBanner from '@/components/basic-components/MainBanner.vue';
 import UiComponents from '@/components/basic-components/AllComponents.vue';
 import UpgradetoPro from '@/components/custom-components/UpgradetoPro.vue';
-import Footer from '@/layouts/footer/Footer.vue';
+import Footer from '@/layouts/footer/Footer.vue'; */
 </script>
 <template>
     <!--Header-->
-    <Header />
+    <!-- <Header />
     <MainBanner />
     <UiComponents />
     <UpgradetoPro />
-    <Footer />
+    <Footer /> -->
 </template>
 
